87af24cedbb2329dd0c6c4631030de30c8bd7095
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-08-02 11:15:46 -0700

    generated files in a dis folder

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -16,6 +16,7 @@ module.exports = () => {
     output: {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
+      publicPath: "./",
     },
     plugins: [
       // Webpack plugin that generates our html file and injects our bundles.
