ae45cc1663f0bb3f9b08b93fc3dea014f1fbd513
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-08-03 12:08:45 -0700

    change webpack to fix errors

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = () => {
 
       // Injects our custom service worker
       new InjectManifest({
-        swSrc: "./client/src-sw.js",
+        swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
 
